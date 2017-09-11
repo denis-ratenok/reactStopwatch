395b37257d4e96da83c176983c5c589e9e7c4189
Denis Ratenok <denis.ratenok@gmail.com>
2017-09-11 18:37:34 +0300

    Lists and Keys

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -5,17 +5,41 @@ import './index.css';
 
 function Time(props) {
 	return (
-		<h1>{props.time}</h1>
+		<h1>{timeFormat(props.time)}</h1>
 	);
 }
 
 
 function Reset(props) {
 	return (
-		<button onClick={props.onClick} className="reset">reset</button>
+		<button onClick={props.onClick} className='reset'>reset</button>
 	);
 }
 
+function ListTime(props) {
+	const times = props.history;
+	const listTimes = times.map((time) =>
+		<li key={time.id}>
+			{timeFormat(time.time)}
+		</li>
+	);
+	return (
+			<ul>{listTimes}</ul>
+	);
+}
+
+function timeFormat(ms) {
+function num(val) {
+    val = Math.floor(val);
+    return val < 10 ? '0' + val : val;
+};
+
+	const	minutes = ms / 6000  % 60;
+  const seconds = ms / 100 % 60;
+  ms = ms % 100;
+
+return num(minutes) + ":" + num(seconds) + ":" + num(ms);
+};
 
 class Stopwatch extends React.Component {
 
@@ -23,9 +47,11 @@ class Stopwatch extends React.Component {
 		super();
 
 		this.state = {
+			id: 0,
 			time: 0,
 			isReady: true,
 			toggleState: 'start',
+			history: [],
 		};
 
 		this.reset = this.reset.bind(this);
@@ -34,25 +60,34 @@ class Stopwatch extends React.Component {
 	}
 
 	startTime() {
-		this.setState({isReady: !this.state.isReady});
-		this.StopwatchId = setInterval(() => this.setState({time: this.state.time+1}), 100);
-		this.setState({ toggleState: 'stop' });
+		this.setState({
+			isReady: !this.state.isReady,
+			toggleState: 'stop',
+		});
+		this.StopwatchId = setInterval(() => this.setState({time: this.state.time+1}), 10);
 		document.querySelector('.reset').style.display = 'block';
 	}
 
 	stopTime() {
-		this.setState({isReady: !this.state.isReady});
-		this.setState({ toggleState: 'start' });
+		const times = this.state.history.slice();
+		times[times.length] = {id: this.state.id, time:this.state.time};
+		this.setState({
+			isReady: !this.state.isReady,
+			toggleState: 'start',
+			history: times,
+			id: this.state.id+1,
+		});
 		clearInterval(this.StopwatchId);
 	}
 
 	reset() {
 		document.querySelector('.reset').style.display = 'none';
 		clearInterval(this.StopwatchId);
-		this.setState({time: 0});
 		this.setState({
 			toggleState: 'start',
 			isReady: true,
+			time: 0,
+			history: [],
 		 });
 	}
 
@@ -66,11 +101,14 @@ class Stopwatch extends React.Component {
 			button = <button onClick={this.stopTime}>{this.state.toggleState}</button>
 		}
 
-		return <div className='stopwatch'>
+		return (
+			<div className='stopwatch'>
 				<Time time={this.state.time} />
 				{button}
 				<Reset onClick = {this.reset} />
+				<ListTime history={this.state.history} />
 			</div>
+		);
 	}
 
 
